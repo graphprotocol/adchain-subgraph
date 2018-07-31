3ec4b80779ffac3b5fa1f9f902fe797153d54ff1
Ford Nickels <ford@thegraph.com>
2018-07-31 16:05:44 -0700

    Use Registry class to get Application data, outcome field -> string

--- mappings/ad-networks.ts
+++ mappings/ad-networks.ts
@@ -1,136 +1,118 @@
+// Respond to application white listed events
 export function applicationWhitelist(event: _ApplicationWhitelisted): void {
   let listingHash = event.params.listingHash.toHex()
 
+  // Bind registry contract
+  let registry = Registry.bind(event.address, event.blockHash)
+  // Use listings method to get application data
+  let listingResult = registry.listings(event.params.listingHash)
+
+  // Create application entity
   let application = new Entity()
   application.setString('id', listingHash)
   application.setBoolean('whitelisted', true)
-  // Apply store updates
+  application.setU256('expirationDate', listingResult.value0)
+  application.setBoolean('whitelisted', listingResult.value1)
+  application.setAddress('owner', listingResult.value2)
+  application.setU256('deposit', listingResult.value3)
+
+  // Apply store updates (insert or update if entity already exists)
   let store = Store.bind(event.blockHash)
   store.set('Application', listingHash, application)
 }
 
+// Respond to application removed events
 export function applicationRemoved(event: _ApplicationRemoved): void {
   let listingHash = event.params.listingHash.toHex()
 
-  let listing = new Entity()
-  listing.setString('id', listingHash)
-
-  // Apply store updates
+  // Remove application from the store
   let store = Store.bind(event.blockHash)
   store.remove('Application', listingHash)
 }
 
-// export function application(event: _Application): void {
-//
-//   let app = new Entity()
-//   app.setString('domain', event.params.domain)
-//   app.setString('deposit', event.params.deposit)
-//
-//   // Apply store updates
-//   let store = Store.bind(event.blockHash)
-//   store.set('Application', event.params.domain.toHash(), app)
-// }
+// Respond to application added events
 export function applicationAdded(event: _Application): void {
+  // Get param data from application event
   let appHash = event.params.listingHash.toHex()
   let appDeposit = event.params.deposit
   let appApplicant = event.params.applicant
   let appEndDate = event.params.appEndDate
 
-  let app = new Entity()
-  app.setString('id', appHash)
-  app.setU256('deposit', appDeposit)
-  app.setAddress('applicant', appApplicant)
-  app.setU256('endDate', appEndDate)
-  app.setBoolean('whitelisted', false)
+  // Create application entity
+  let application = new Entity()
+  application.setString('id', appHash)
+  application.setU256('deposit', appDeposit)
+  application.setAddress('applicant', appApplicant)
+  application.setU256('endDate', appEndDate)
+  application.setBoolean('whitelisted', false)
 
   // Apply store updates
   let store = Store.bind(event.blockHash)
-  store.set('Application', appHash, app)
+  store.set('Application', appHash, application)
 }
 
+// Respond to challange submitted events
 export function challenge(event: _Challenge): void {
+// Get param data from challenge event
   let listingHash = event.params.listingHash.toHex()
   let challengeId = event.params.challengeID
   let commitEndDate = event.params.commitEndDate
   let revealEndDate = event.params.revealEndDate
   let challenger = event.params.challenger
 
+  // Create challenge entity
   let challenge = new Entity()
-  challenge.setU256('id', challengeId)
+  challenge.setString('id', listingHash)
+  challenge.setU256('challengeId', challengeId)
   challenge.setU256('commitEndDate', commitEndDate)
   challenge.setU256('revealEndDate', revealEndDate)
   challenge.setAddress('challenger', challenger)
   challenge.setString('application', listingHash)
-  challenge.setString('data', event.params.data)
-
-  let application = new Entity()
-  application.setString('challenge', listingHash)
+  challenge.setString('outcome', 'pending')
 
   // Apply store updates
   let store = Store.bind(event.blockHash)
   store.set('Challenge', challengeId.toHex(), challenge)
-  store.set('Application', listingHash, application)
 }
 
+// Respond to challenge succeeded events
 export function challengeSucceeded(event: _ChallengeSucceeded): void {
+  // Get param data from challenge succeeded event
   let challengeHash = event.params.listingHash.toHex()
   let challengeId = event.params.challengeID
   let rewardPool = event.params.rewardPool
   let totalTokens = event.params.totalTokens
 
+  // Create success entity
   let success = new Entity()
-  success.setU256('id', challengeId)
+  success.setString('id', challengeHash)
+  success.setU256('challengeId', challengeId)
   success.setU256('rewardPool', rewardPool)
   success.setU256('totalTokens', totalTokens)
-  success.setBoolean('outcome', true)
+  success.setString('outcome', 'success')
 
   // Apply store updates
   let store = Store.bind(event.blockHash)
   store.set('Challenge', challengeId.toHex(), success)
 }
 
+// Respond to challenge failed events
 export function challengeFailed(event: _ChallengeFailed): void {
+  // Get param data from challenge succeeded event
   let challengeHash = event.params.listingHash.toHex()
   let challengeId = event.params.challengeID
   let rewardPool = event.params.rewardPool
   let totalTokens = event.params.totalTokens
 
+  // Create fail entity
   let fail = new Entity()
-  fail.setU256('id', challengeId)
+  fail.setString('id', challengeHash)
+  fail.setU256('challengeId', challengeId)
   fail.setU256('rewardPool', rewardPool)
   fail.setU256('totalTokens', totalTokens)
-  fail.setBoolean('outcome', false)
+  fail.setString('outcome', 'failed')
 
   // Apply store updates
   let store = Store.bind(event.blockHash)
   store.set('Challenge', challengeId.toHex(), fail)
-}
-
-export function deposit(event: _Deposit): void {
-  let depositHash = event.params.listingHash.toHex()
-  let added = event.params.added
-  let newTotal = event.params.newTotal
-  let depositAddress = event.params.owner
-
-  let deposit = new Entity()
-  deposit.setU256('amountAdded', added)
-  deposit.setU256('newTotal', newTotal)
-  deposit.setAddress('address', depositAddress)
-
-  let store = Store.bind(event.blockHash)
-  store.set('Deposit', depositHash, deposit)
-}
-export function withdrawal(event: _Withdrawal): void {
-  let withdrawHash = event.params.listingHash.toHex()
-  let withdrew = event.params.withdrew
-  let newTotal = event.params.newTotal
-  let withdrawAddress = event.params.owner
-
-  let deposit = new Entity()
-  deposit.setU256('amountAdded', withdrew)
-  deposit.setU256('newTotal', newTotal)
-  deposit.setAddress('address', withdrawAddress)
-
-  let store = Store.bind(event.blockHash)
-  store.set('Withdrawal', withdrawHash, deposit)
 }
