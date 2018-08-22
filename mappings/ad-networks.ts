f11a27ab27ec8f150e85b3b2f4b13e9570f98d90
Ford Nickels <ford@thegraph.com>
2018-08-22 11:33:49 -0700

    mappings: Handlers for all primary contract events, remove store.bind
    New event handlers created: application, deposit, withdrawal,
    listingRemoved, listingWithdrawn, rewardClaimed, touchAndRemoved.
    New entities created to be used by the new handlers: Deposit, Reward,
    Withdrawal, User
    BlockHash does not need to be bound to the store any longer.

--- mappings/ad-networks.ts
+++ mappings/ad-networks.ts
@@ -4,6 +4,7 @@ export function applicationWhitelist(event: _ApplicationWhitelisted): void {
 
   // Bind registry contract
   let registry = Registry.bind(event.address, event.blockHash)
+
   // Use listings method to get application data
   let listingResult = registry.listings(event.params.listingHash)
 
@@ -16,8 +17,12 @@ export function applicationWhitelist(event: _ApplicationWhitelisted): void {
   application.setAddress('owner', listingResult.value2)
   application.setU256('deposit', listingResult.value3)
 
+  let user = new Entity()
+  user.setAddress('address', listingResult.value2)
+
   // Apply store updates (insert or update if entity already exists)
   store.set('Application', listingHash, application)
+  store.set('User', listingResult.value2.toHex(), user)
 }
 
 // Respond to application removed events
@@ -65,6 +70,10 @@ export function challenge(event: _Challenge): void {
   challenge.setAddress('challenger', challenger)
   challenge.setString('application', listingHash)
   challenge.setString('outcome', 'pending')
+  challenge.setBoolean('rewardClaimed', false)
+
+  let user = new Entity()
+  user.setAddress('address', challenger)
 
   // Apply store updates
   store.set('Challenge', challengeId.toHex(), challenge)
@@ -100,7 +109,6 @@ export function challengeFailed(event: _ChallengeFailed): void {
 
   // Create fail entity
   let fail = new Entity()
-  // fail.setString('id', challengeHash)
   fail.setU256('id', challengeId)
   fail.setU256('rewardPool', rewardPool)
   fail.setU256('totalTokens', totalTokens)
@@ -109,4 +117,89 @@ export function challengeFailed(event: _ChallengeFailed): void {
 
   // Apply store updates
   store.set('Challenge', challengeId.toHex(), fail)
+}
+
+// Respond to deposit events
+export function deposit(event: _Deposit): void {
+  // Get param data from deposit event
+  let listingHash = event.params.listingHash.toHex()
+  let added = event.params.added
+  let newTotal = event.params.newTotal
+  let owner = event.params.owner
+  let depositId = listingHash + '_' + owner.toHex()
+
+  // Create fail entity
+  let deposit = new Entity()
+  deposit.setString('id', depositId)
+  deposit.setU256('added', added)
+  deposit.setU256('newTotal', newTotal)
+  deposit.setString('owner', owner.toHex())
+
+  let user = new Entity()
+  user.setAddress('address', owner)
+
+  // Apply store updates
+  store.set('Challenge', depositId, deposit)
+  store.set('User', owner.toHex(), user)
+}
+
+// Respond to withdrawal events
+export function withdrawal(event: _Withdrawal): void {
+  // Get param data from withdrawal event
+  let listingHash = event.params.listingHash.toHex()
+  let withdrew = event.params.withdrew
+  let newTotal = event.params.newTotal
+  let owner = event.params.owner
+  let withdrawId = listingHash + '_' + owner.toHex()
+
+  // Create fail entity
+  let withdrawal = new Entity()
+  withdrawal.setString('id', withdrawId)
+  withdrawal.setU256('withdrew', withdrew)
+  withdrawal.setU256('newTotal', newTotal)
+  withdrawal.setString('owner', owner.toHex())
+
+  let user = new Entity()
+  user.setAddress('address', owner)
+
+  // Apply store updates
+  store.set('Withdrawal', withdrawId, withdrawal)
+  store.set('User', owner.toHex(), user)
+}
+
+// Respond to listing removed events
+export function listingRemoved(event: _ListingRemoved): void {
+  // Get param data from listing removed event
+  let listingHash = event.params.listingHash.toHex()
+
+  // Apply store updates
+  store.remove('Application', listingHash)
+}
+
+// Respond to listing withdrawn events
+export function listingWithdrawn(event: _ListingWithdrawn): void {
+  // Get param data from listingWithdrawn event
+  let listingHash = event.params.listingHash.toHex()
+
+  // Apply store updates
+  store.remove('Application', listingHash)
+}
+
+// Respond to reward claimed events
+export function rewardClaimed(event: _RewardClaimed): void {
+  // Get param data from rewardClaimed event
+  let challengeId = event.params.challengeID
+  let voter = event.params.voter
+
+  // Create challenge reward entity
+  let challengeReward = new Entity()
+  challengeReward.setAddress('voter', voter)
+  challengeReward.setBoolean('rewardClaimed', true)
+
+  let user = new Entity()
+  user.setAddress('address', voter)
+
+  // Apply store updates
+  store.set('Challenge', challengeId.toHex(), challengeReward)
+  store.set('User', voter.toHex(), user)
 }
